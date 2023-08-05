754d1ea6ea48bca524eae4fd70d23505afad9b4c
arunachalam3342 <105549877+arunachalam3342@users.noreply.github.com>
2023-08-05 17:46:09 +0530

    youtube project addded

--- src/components/Project/Projectpage.js
+++ src/components/Project/Projectpage.js
@@ -5,11 +5,11 @@ import { Container, Row, Col } from 'react-bootstrap';
 import img01 from "../../Assets/task01.png";
 import img02 from "../../Assets/task02.png";
 import img03 from "../../Assets/task03.png";
-import img04 from "../../Assets/task04.png";
 import img05 from "../../Assets/task05.png";
 import img06 from "../../Assets/task06.png";
 import Real_Estate from "../../Assets/Real_Estate.png";
 import To_Do_List from "../../Assets/To-Do-List.png";
+import youtube from "../../Assets/Youtube.png";
 
 function Projectpage() {
     return (
@@ -19,6 +19,9 @@ function Projectpage() {
                     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
                         <Col md={4} className="project-card">
                             <ProjectCard imgPath={Real_Estate} title="REAL ESTATE PRICE PREDICTION" ghLink="https://arunachalam3342-fundamentalsofdataanalyt-real-estate-app-i0wgpi.streamlit.app/" />
+                        </Col>
+                        <Col md={4} className="project-card">
+                            <ProjectCard imgPath={youtube} title="YOUTUBE CLONE" ghLink="https://arunachalam3342.github.io/Youtube-Clone/" />
                         </Col>
                         <Col md={4} className="project-card">
                             <ProjectCard imgPath={To_Do_List} title="To-Do-List" ghLink="https://arunachalam3342.github.io/To-Do-List/" />
@@ -32,9 +35,7 @@ function Projectpage() {
                         <Col md={4} className="project-card">
                             <ProjectCard imgPath={img03} title="COLOR PICKER" ghLink="https://arunachalam3342.github.io/React-Task03/" />
                         </Col>
-                        <Col md={4} className="project-card">
-                            <ProjectCard imgPath={img04} title="CLIENTSIDE DISPLAY" ghLink="https://arunachalam3342.github.io/React-Task04/" />
-                        </Col>
+
                         <Col md={4} className="project-card">
                             <ProjectCard imgPath={img05} title="AGE CALCULATOR" ghLink="https://arunachalam3342.github.io/React-Task05/" />
                         </Col>
